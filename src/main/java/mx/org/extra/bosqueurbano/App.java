884faaf3c14c68cbf157a81730b874f64a6c5761
Ignacio Arces <arcesino@gmail.com>
2015-07-21 17:27:46 -0500

    API skeleton

--- src/main/java/mx/org/extra/bosqueurbano/App.java
+++ src/main/java/mx/org/extra/bosqueurbano/App.java
@@ -8,6 +8,16 @@ import static spark.Spark.*;
 public class App {
 
   public static void main(String[] args) {
-    get("/hello", (req, res) -> "Hello World");
+    get("/", (req, res) -> "Home");
+
+    post("/trees/:id/reports", (req, res) -> "Reports of a tree");
+
+    get("/species/:id", (req, res) -> "Info of a specie");
+
+    get("/trees/:id", (req, res) -> "Info of a tree");
+
+    post("/treekeepers", (req, res) -> "Tree keepers of a tree");
+
+    get("/trees", (req, res) -> "All my trees");
   }
 }
